ad3beffcf1c2279edb19604349b9f9f28e79d557
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-07-29 01:40:30 +0300

    Implement localStorage

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -13,10 +13,12 @@ export class ProductsComponent implements OnInit {
   public products!: Product[];
   public selectedProductId: number | null = null;
 
+  readonly LOCAL_PRODUCTS: string = 'products';
+
   constructor(private fakeStore: FakeStoreService) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.initProducts();
   }
 
   public getProductById(id: number): Product {
@@ -27,17 +29,46 @@ export class ProductsComponent implements OnInit {
     this.selectedProductId = productId;
   }
   public handleProductRemove(productId: number): void {
-    this.products = <Product[]>this.products.filter((product: Product): boolean => product.id !== productId);
     this.selectedProductId = null;
+    this.products = <Product[]>this.products.filter((product: Product): boolean => product.id !== productId);
+
+    localStorage.setItem(this.LOCAL_PRODUCTS, JSON.stringify(this.products));
   }
 
   public handleModalClose(status: null): void {
     this.selectedProductId = status;
   }
 
+  public loadProductsFromApi(): void {
+    this.getProducts();
+  }
+
+  public removeAllProducts(): void {
+    this.products = [];
+    localStorage.setItem(this.LOCAL_PRODUCTS, JSON.stringify([]))
+  }
+
   private getProducts(): void {
     this.getProductsSub = <Subscription>this.fakeStore.getProducts().subscribe((products: Product[]): void => {
       this.products = products;
+      localStorage.setItem(this.LOCAL_PRODUCTS, JSON.stringify(products));
     });
+  }
+
+  private initProducts(): void {
+    const productsFromLocalStorage: string | null = localStorage.getItem(this.LOCAL_PRODUCTS);
+    let savedProducts!: Product[];
+
+    if (productsFromLocalStorage) {
+      savedProducts = JSON.parse(<string>productsFromLocalStorage);
+    }
+
+    if (savedProducts && savedProducts.length >= 0) {
+      this.products = savedProducts;
+    }
+
+    if (!savedProducts) {
+      this.getProducts();
+    }
   }
 }
